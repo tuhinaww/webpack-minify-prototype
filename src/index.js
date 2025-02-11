8846071a7ec653dc6bc924ba594978cb7b05d5d6
tuhinaww <iceybubblegumtea@gmail.com>
2025-02-11 22:15:22 +0530

    test-2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import "./style.css"; // ✅ Webpack will now detect and process the CSS file
 
-console.log("Another Day, Another Test!");
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector("form").addEventListener("submit", (event) => {
+        event.preventDefault();
+        alert("Thank you for reaching out! I'll get back to you soon.");
+    });
+});
